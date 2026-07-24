4429a114b80745ebb7aa795cb6a7e5f4d24c9567
thebase16 <hexadecimal@live.com>
2026-07-24 10:38:34 -0300

    Add scroll event listener to update gold element styles

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const gold = document.querySelector(".gold");
+
+window.addEventListener("scroll", () => {
+
+    const y = window.scrollY;
+
+    gold.style.setProperty("--lightY", `${20 + y * 0.02}%`);
+    gold.style.setProperty("--lightX", `${50 + Math.sin(y/250)*8}%`);
+
+});
